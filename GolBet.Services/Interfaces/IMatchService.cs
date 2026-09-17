13930757b3e7f12888da29acd5f2bb63e0af8812
Sebastian Rojas <sebastianrojaso2006@gmail.com>
2026-09-17 00:53:27 -0500

    Add match detail page and board filters (Module 5)

--- GolBet.Services/Interfaces/IMatchService.cs
+++ GolBet.Services/Interfaces/IMatchService.cs
@@ -5,8 +5,9 @@ using GolBet.Services.DTOs;
 namespace GolBet.Services.Interfaces;
 
 public interface IMatchService
-
 {
     /// <summary>Match board: all active matches ordered by date.</summary> 
     Task<IEnumerable<MatchDto>> GetBoardAsync(MatchStatus? status = null);
+
+    Task<MatchDetailDto?> GetDetailAsync(int id);
 }
